b5cdb78bada3274f914e787acd468425c5bb5cd4
fradojr <fradoforumm@gmail.com>
2021-06-20 10:00:43 +0700

    instan commit

---
+++ ff_pt_count/ff-hitung.js
@@ -0,0 +1,7 @@
+document.getElementById("hitung").
+addEventListener("click", hitung);
+
+function hitung(){
+    let namaTurnamen=document.getElementById("namaTurnamen").value;
+    document.getElementById("inputValue12m3k").innerHTML=namaTurnamen;
+}
