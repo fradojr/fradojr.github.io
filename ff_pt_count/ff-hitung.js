228c4f5f5567c331277ae3f672217ab8b62767f9
fradojr <fradoforumm@gmail.com>
2021-06-20 10:38:13 +0700

    instan commit

--- ff_pt_count/ff-hitung.js
+++ ff_pt_count/ff-hitung.js
@@ -1,7 +1,44 @@
-document.getElementById("hitung").
-addEventListener("click", hitung);
+document.getElementById("simpan").addEventListener("click", simpan)
 
-function hitung(){
-    let namaTurnamen=document.getElementById("namaTurnamen").value;
-    document.getElementById("inputValue12m3k").innerHTML=namaTurnamen;
+function simpan(){
+    let namaTurnamen = document.getElementById("namaTurnamen").value
+    document.getElementById("namaTurnamenHasil").innerHTML = namaTurnamen
+
+    let inputName1 = document.getElementById("inputName1").value
+    document.getElementById("outputName1").innerHTML = inputName1
+    
+    let inputName2 = document.getElementById("inputName2").value
+    document.getElementById("outputName2").innerHTML = inputName2
+    
+    let inputName3 = document.getElementById("inputName3").value
+    document.getElementById("outputName3").innerHTML = inputName3
+    
+    let inputName4 = document.getElementById("inputName4").value
+    document.getElementById("outputName4").innerHTML = inputName4
+    
+    let inputName5 = document.getElementById("inputName5").value
+    document.getElementById("outputName5").innerHTML = inputName5
+    
+    let inputName6 = document.getElementById("inputName6").value
+    document.getElementById("outputName6").innerHTML = inputName6
+    
+    let inputName7 = document.getElementById("inputName7").value
+    document.getElementById("outputName7").innerHTML = inputName7
+    
+    let inputName8 = document.getElementById("inputName8").value
+    document.getElementById("outputName8").innerHTML = inputName8
+    
+    let inputName9 = document.getElementById("inputName9").value
+    document.getElementById("outputName9").innerHTML = inputName9
+    
+    let inputName10 = document.getElementById("inputName10").value
+    document.getElementById("outputName10").innerHTML = inputName10
+    
+    let inputName11 = document.getElementById("inputName11").value
+    document.getElementById("outputName11").innerHTML = inputName11
+    
+    let inputName12 = document.getElementById("inputName12").value
+    document.getElementById("outputName12").innerHTML = inputName12
+    
+    
 }
